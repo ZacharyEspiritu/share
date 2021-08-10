3c51b1347982044bfb172c88d2926542a3c2dcdd
lucyq <lucyzqin@gmail.com>
2021-08-10 12:00:24 -0400

    init

--- js/client/party.js
+++ js/client/party.js
@@ -1,34 +1,55 @@
 server_ip = "localhost:8083"
 oprf_ip = "localhost:8082"
 
+analystPublicKey = "dac56ba60fe8696b97d24a0b7a88f1bb";
+
 function run() {
     console.log("HELLOWORLD!")
-    $.post('http://' + server_ip + '/setup', {
-        "schema": "schema",
-        "emm_filter": "emm filter",
-        "edx_data": "edx data"
-      }, function(response) {
-        console.log("Server setup response:", response);
-        // const timeEnd = performance.now();
-        // console.log("Total time (ms): ", timeEnd - timeStart);
-    });
+    init()
+    
 
-    $.post('http://' + oprf_ip + '/oprf', {
-        "input": "input",
-      }, function(response) {
-        console.log("OPRF response:", response);
-        // const timeEnd = performance.now();
-        // console.log("Total time (ms): ", timeEnd - timeStart);
-    });
+
     
 }
 
 function init() {
-  
+  $.post('http://' + server_ip + '/postAnalystPublicKey', {
+    "analystId": "analyst",
+    "publicKey": analystPublicKey
+  }, function(response) {
+    console.log("Server setup response:", response);
+    // const timeEnd = performance.now();
+    // console.log("Total time (ms): ", timeEnd - timeStart);
+  });
+
+  $.post('http://' + server_ip + '/retrieveAnalystPublicKey', {
+    "analystId": "analyst",
+  }, function(response) {
+    console.log("Analyst public key:", response);
+    // const timeEnd = performance.now();
+    // console.log("Total time (ms): ", timeEnd - timeStart);
+  });
+
 }
 
 function setup() {
 
+  $.post('http://' + server_ip + '/setup', {
+    "schema": "schema",
+    "emm_filter": "emm filter",
+    "edx_data": "edx data"
+  }, function(response) {
+    console.log("Server setup response:", response);
+    // const timeEnd = performance.now();
+    // console.log("Total time (ms): ", timeEnd - timeStart);
+});
+$.post('http://' + oprf_ip + '/oprf', {
+  "input": "input",
+}, function(response) {
+  console.log("OPRF response:", response);
+  // const timeEnd = performance.now();
+  // console.log("Total time (ms): ", timeEnd - timeStart);
+});
 }
 
 
--- js/server.js
+++ js/server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8083;
 const path = require('path');
 
+
+let analystPublicKeys = {};
 // const clusion = require('./clusion');
 
 app.use(express.static(__dirname + '/client'));
@@ -20,6 +22,23 @@ app.get('/', async function(req,res){
   res.sendFile((path.join(__dirname + '/client/index.html')));
 });
 
+app.post('/postAnalystPublicKey', async function(req, res) {
+  var analystPublicKey = req.body.publicKey;
+  var analystId = req.body.analystId;
+  analystPublicKeys[analystId] = analystPublicKey;
+  console.log("Storing analyst public key: ", analystId, analystPublicKey);
+});
+
+app.post('/retrieveAnalystPublicKey', async function(req, res) {
+  var analystId = req.body.analystId;
+
+  console.log("Retrieving analyst public key", analystId)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  console.log("Successful setup")
+  res.status(200).send(JSON.stringify(analystPublicKeys[analystId]));
+
+});
 
 app.post('/setup', async function(req,res) {  
   var schema = req.body.schema;
